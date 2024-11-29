38e35a2d2d5600c4a86fe31df53ee2a8b6173e58
Rohit Swami <121743390+Rohitswami16@users.noreply.github.com>
2024-11-29 17:56:29 +0530

    Update 16_Higher_order_function.js

--- 16_Higher_order_function.js
+++ 16_Higher_order_function.js
@@ -1,1 +1,19 @@
+// Using Callback Functions
 
+// Function to display data
+function display(data) {
+    console.log("data " + data); // Logs the data to the console
+}
+
+// Function to add two numbers and display the result using a callback
+function add(a, b, show) {
+    let res = a + b; // Calculate the sum
+    show(res); // Call the callback function with the result
+}
+
+// Call add with numbers and the display function
+add(10, 20, display); 
+// Output:
+// data 30
+
+// -----------------------------------------------------------------------------------------------------------------------------------------------------------------
